d13419de068b2ae8399de0cc97478dfced50e3bf
zhouning <2267431887@qq.com>
2019-06-03 10:26:06 +0800

    添加配置中心仓库

---
+++ config-server/src/main/java/ning/zhou/study/springcloud/config/servergit/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package ning.zhou.study.springcloud.config.servergit.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
